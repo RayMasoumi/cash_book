c58773f5cc480cc69d6d1e1907ed024a96ca91a8
azinmapar <79557566+azinmapar@users.noreply.github.com>
2023-07-02 14:39:18 +0330

    translation half done

---
+++ lib/interntionalisation/translate.dart
@@ -0,0 +1,31 @@
+import 'package:get/get.dart';
+
+class Translate implements Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+        'en': {
+          "Track Income & Expenses": "Track Income & Expenses",
+          "Manage Your Business with Staff": "Manage Your Business with Staff",
+          "Set-Up Multiple Businesses": "Set-Up Multiple Businesses",
+          "Stay updated on your profits": "Stay updated on your profits",
+          "Add team members and assign roles":
+              "Add team members and assign roles",
+          "Keep multiple business records on one app":
+              "Keep multiple business records on one app",
+          'Skip': 'Skip',
+          'Done': 'Done',
+        },
+        'fa': {
+          "Track Income & Expenses": "ثبت دریافتی‌ها و پرداختی‌ها",
+          "Manage Your Business with Staff": "کسب‌و‌کارتان را",
+          "Set-Up Multiple Businesses": "Set-Up Multiple Businesses",
+          "Stay updated on your profits": "Stay updated on your profits",
+          "Add team members and assign roles":
+              "Add team members and assign roles",
+          "Keep multiple business records on one app":
+              "Keep multiple business records on one app",
+          'Skip': 'Skip',
+          'Done': 'Done',
+        },
+      };
+}
