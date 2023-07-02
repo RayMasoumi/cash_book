bef4436cd75a8cb78e2bf50554e00ddd39d7fd45
Ray Masoumi <Ray.masoumi80@gmail.com>
2023-07-02 12:51:15 +0330

    ListView of cards added >> colorful

--- lib/widgets/card_list_view_widget_books_list.dart
+++ lib/widgets/card_list_view_widget_books_list.dart
@@ -1,32 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-import '../model/user.dart';
+import '../constants/colors.dart';
 
 class CardListViewWidgetBooksList extends StatelessWidget {
-  CardListViewWidgetBooksList({
-    super.key,
-  });
-
   // * testing book values:
-  final User currentUser = User(
-    userId: 'userId',
-    userName: 'userName',
-    userPhoneNumber: 'userPhoneNumber',
-    userEmail: 'userEmail',
-    lastSyncDate: 'lastSyncDate',
-    userCreationDate: 'userCreationDate',
-  );
+  // final User currentUser = User(
+  //   userId: 'userId',
+  //   userName: 'userName',
+  //   userPhoneNumber: 'userPhoneNumber',
+  //   userEmail: 'userEmail',
+  //   lastSyncDate: 'lastSyncDate',
+  //   userCreationDate: 'userCreationDate',
+  // );
+
+  final List<String> test = ['1', '2', '3', '4', '5', '6', '7'];
+
+  CardListViewWidgetBooksList({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // currentUser.userBooks.add(
+    //   Book(
+    //       bookId: 'bookId',
+    //       bookName: 'bookName',
+    //       privateBook: true,
+    //       bookLastModified: 'bookLastModified',
+    //       bookOwnerUserId: 'bookOwnerUserId'),
+    // );
+    // currentUser.userBooks.add(
+    //   Book(
+    //       bookId: 'bookId',
+    //       bookName: 'bookName',
+    //       privateBook: true,
+    //       bookLastModified: 'bookLastModified',
+    //       bookOwnerUserId: 'bookOwnerUserId'),
+    // );
+    // currentUser.userBooks.add(
+    //   Book(
+    //       bookId: 'bookId',
+    //       bookName: 'bookName',
+    //       privateBook: true,
+    //       bookLastModified: 'bookLastModified',
+    //       bookOwnerUserId: 'bookOwnerUserId'),
+    // );
+
+    // * a method to choose color:
+    Color getColor(int index) {
+      Color color;
+      switch (index % 5) {
+        case 0:
+          {
+            color = kPastel1Color;
+          }
+          break;
+        case 1:
+          {
+            color = kPastel5Color;
+          }
+          break;
+        case 2:
+          {
+            color = kPastel4Color;
+          }
+          break;
+        case 3:
+          {
+            color = kPastel3Color;
+          }
+          break;
+        case 4:
+          {
+            color = kPastel2Color;
+          }
+          break;
+        default:
+          {
+            color = kPastel1Color;
+          }
+          break;
+      }
+      return color;
+    }
+
     return SizedBox(
       width: Get.width,
       child: ListView.builder(
-          itemCount: 10,
+          itemCount: test.length,
           itemBuilder: (context, index) {
-            return const Card(
-              child: ListTile(),
+            return Card(
+              color: getColor(index),
+              child: const ListTile(),
             );
           }),
     );
